f7526536c5af76caa91091d84512cb7a5ebf0d3b
ana-developer-beatriz <anabeatrizgithub@gmail.com>
2023-04-24 21:36:16 -0300

    add page home

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+function App() {
+  const meuArray = ['Valor 1', 'Valor 2', 'Valor 3']
+  const pages = ['www.google.com', 'www.google.com', 'www.google.com']
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Home/>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
