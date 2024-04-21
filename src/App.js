53717ea58cf9f21bf3c2f55cc6498893fb7f16f1
ana-developer-beatriz <anabeatrizgithub@gmail.com>
2024-04-21 17:44:04 -0300

    update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Site da Anana em breve =D
         </p>
         <a
           className="App-link"
